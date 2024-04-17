6a1756b2894e981bcace203d2e74dab2e42f93fb
adilmangla8 <adil.ahmad@ucp.edu.pk>
2024-04-17 15:21:18 +0100

    Initial Commit

---
+++ src/utils/cardData.ts
@@ -0,0 +1,22 @@
+export const data: any[] = [
+  {
+    id: 1,
+    title: "Notes 1",
+    description: "This is a note we have saved",
+  },
+  {
+    id: 2,
+    title: "Notes 2",
+    description: "This is a note we have saved",
+  },
+  {
+    id: 3,
+    title: "Notes 3",
+    description: "This is a note we have saved",
+  },
+  {
+    id: 4,
+    title: "Notes 4",
+    description: "This is a note we have saved",
+  },
+];
